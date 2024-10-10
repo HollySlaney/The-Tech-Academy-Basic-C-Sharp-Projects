8219179d259d1a7ed6dab954ceac4a9b7f7d3e5b
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-10-09 20:33:31 -0600

    Submitted Method Class Assignment

---
+++ MethodClassAssignment/MethodClassAssignment/MathOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+     public class MathOperation
+    {
+        //Void method with two integers as parameters
+        public void mathOperation(int a, int b)
+        {
+            //Math operation on first integer
+            int result = a * 5;
+            //Display the second integer
+            Console.WriteLine("The second integer is: " + b);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the MathOperation class
+            MathOperation math1 = new MathOperation();
+            //Call the method passing in two numbers
+            math1.mathOperation(10, 50);
+            //Call the method parameters by name
+            math1.mathOperation(a: 10, b: 50);
+            Console.ReadLine();
+        }
+    }
+}
