7a20b93903b60be0b0da54145245d5b6e5f8f810
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-10-15 18:13:48 -0600

    Submitted Abstract Class Assignment

---
+++ AbstractClassAssignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    //Make Employee class inherit from Person class
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+        //Implement teh SayName() method inside of Employee class
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ AbstractClassAssignment/AbstractClassAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate an Employee object
+            Employee sam = new Employee() { firstName = "Sample", lastName = " Student" };
+            //Call SayName method on object
+            sam.SayName();
+            Console.ReadLine();
+        }
+    }
+}
