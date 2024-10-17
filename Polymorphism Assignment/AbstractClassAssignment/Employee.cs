511f3927fc238ead3e7660659c452ecba805ce09
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-10-16 22:05:39 -0600

    Submitted Polymorphism Assignment

---
+++ Polymorphism Assignment/AbstractClassAssignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    //Make Employee class inherit from Person class
+    //Inherit IQuittable interface
+    public class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+        //Implement the SayName() method inside of Employee class
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+        //Implement Quit() method
+        public void Quit()
+        {
+            Console.WriteLine("Employee has quit.");
+        }
+    }
+}
---
+++ Polymorphism Assignment/AbstractClassAssignment/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassAssignment
+{
+    interface IQuittable
+    {
+        //Create interface called IQuittable and define void method called Quit()
+        void Quit();
+    }
+}
