a1980dc284f16d3f9aabe5eeb193c446ad7179c5
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-10-21 11:59:28 -0600

    Submitted Operators Assignment

---
+++ OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //Overloading the == operator
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            //Compare Id properties
+            return employee1.Id == employee2.Id;
+
+        }
+        //Overloading the != operator as the overloading pair to == operator
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return !(employee1 == employee2);
+        }
+    }
+}
---
+++ OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate Employees and assign values to their properties
+            Employee employee1 = new Employee
+            {
+                Id = 1,
+                firstName = "Holly",
+                lastName = "Slaney"
+            };
+
+            Employee employee2 = new Employee
+            {
+                Id = 2,
+                firstName = "Stephanie",
+                lastName = "Parker"
+            };
+
+            //Compare the two employees using == overloaded operator and display results
+            if (employee1 == employee2)
+            {
+                Console.WriteLine("The two employees are equal.");
+            }
+            //Compare employees using the != overloaded operator and display results
+            if (employee1 != employee2)
+            {
+                Console.WriteLine("The two employees are not equal.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
