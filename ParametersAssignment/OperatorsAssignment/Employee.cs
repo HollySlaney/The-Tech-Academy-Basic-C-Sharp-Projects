4b10b04f5279cd3338a64b41411e8bb8c254c322
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-10-22 22:49:50 -0600

    Submitted Parameters Assignment

---
+++ ParametersAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee<T>
+    {
+        public T Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        //List of items of generic type T
+        public List<T> Things { get; set; }
+
+        //Overloading the == operator
+        public static bool operator ==(Employee<T> employee1, Employee<T> employee2)
+        {
+            //Compare Id properties
+            return EqualityComparer<T>.Default.Equals(employee1.Id, employee2.Id);
+
+        }
+        //Overloading the != operator as the overloading pair to == operator
+        public static bool operator !=(Employee<T> employee1, Employee<T> employee2)
+        {
+            return !(employee1 == employee2);
+        }
+    }
+}
---
+++ ParametersAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate Employee object with string data type as its generic parameter
+            Employee<string> stringEmployee = new Employee<string>
+            {
+                Id = "FVH566",
+                firstName = "Holly",
+                lastName = "Slaney",
+                Things = new List<string> { "Name Tag", "Vest", "Tie" }
+            };
+
+            Employee<int> intEmployee = new Employee<int>
+            {
+                Id = 325,
+                firstName = "Stephanie",
+                lastName = "Parker",
+                Things = new List<int> { 654, 475, 978 }
+            };
+
+            //Loop through and print all the things for stringEmployee
+            Console.WriteLine("Things for Employee " + stringEmployee.firstName + " " + stringEmployee.lastName + " (Id: " + stringEmployee.Id + "):");
+            foreach (var thing in stringEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine();
+
+            //Loop through and print all the things for intEmployee
+            Console.WriteLine("Things for Employee " + intEmployee.firstName + " " + intEmployee.lastName + " (Id: " + intEmployee.Id + "):");
+            foreach (var thing in intEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
