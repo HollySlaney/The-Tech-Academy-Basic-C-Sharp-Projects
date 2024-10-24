78a73689d6f90ddecef7bf13d76b3c3324f70c84
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-10-23 21:53:16 -0600

    Submitted Struct Assignment

---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    public struct Number
+    {
+        public decimal Amount { get; set; }
+        public Number(decimal amount)
+        {
+            Amount = amount;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an object of data type Number and assign an amount to it
+            Number myNumber = new Number(123.45m);
+            //Display the amount
+            Console.WriteLine("The amount is: " + myNumber.Amount);
+            Console.ReadLine();
+        }
+    }
+}
