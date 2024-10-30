2f4613358aba9c5b1cf4b477344b60cfcdf9e1a2
HollySlaney <159870624+HollySlaney@users.noreply.github.com>
2024-10-30 14:33:19 -0600

    Submitted Input Assignment

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //File path where the number will be logged
+            string filePath = @"C:\Users\Mom\Logs\log.txt";
+            //Ask the user for a number
+            Console.WriteLine("Please input a number between 1 and 100.");
+            int answer = int.Parse(Console.ReadLine());
+            //Logs number to file
+            File.WriteAllText(filePath, answer.ToString());
+            //Print text file back to user
+            string loggedNumber = File.ReadAllText(filePath);
+            Console.WriteLine("The logged number is: " + loggedNumber);
+            
+            Console.ReadLine();
+
+        }
+    }
+}
